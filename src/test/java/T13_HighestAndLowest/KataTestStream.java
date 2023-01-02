4fc3842e8246139fef583e27466b934c83cb555f
iskamele <iskamele@gmail.com>
2023-01-02 11:59:56 +0200

    Highest and Lowest

--- src/test/java/T13_HighestAndLowest/KataTestStream.java
+++ src/test/java/T13_HighestAndLowest/KataTestStream.java
@@ -7,10 +7,10 @@ import static org.junit.Assert.assertEquals;
 public class KataTestStream {
   @Test
   public void test1() {
-    assertEquals("42 -9", Kata.highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+    assertEquals("42 -9", KataStream.highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
   }
   @Test
   public void test2() {
-    assertEquals("3 1", Kata.highAndLow("1 2 3"));
+    assertEquals("3 1", KataStream.highAndLow("1 2 3"));
   }
 }
