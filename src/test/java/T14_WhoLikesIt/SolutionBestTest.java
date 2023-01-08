0016fca7d6fe1e76430a3fa5cbf7304368f657c8
iskamele <iskamele@gmail.com>
2023-01-08 23:27:11 +0200

    Who likes it?

---
+++ src/test/java/T14_WhoLikesIt/SolutionBestTest.java
@@ -0,0 +1,16 @@
+package T14_WhoLikesIt;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionBestTest {
+    @Test
+    public void staticTests() {
+        assertEquals("no one likes this", SolutionBest.whoLikesIt());
+        assertEquals("Peter likes this", SolutionBest.whoLikesIt("Peter"));
+        assertEquals("Jacob and Alex like this", SolutionBest.whoLikesIt("Jacob", "Alex"));
+        assertEquals("Max, John and Mark like this", SolutionBest.whoLikesIt("Max", "John", "Mark"));
+        assertEquals("Alex, Jacob and 2 others like this", SolutionBest.whoLikesIt("Alex", "Jacob", "Mark", "Max"));
+    }
+}
