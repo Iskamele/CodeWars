96cf66754a01f9289f65e4c48b67a249e89d5f43
iskamele <iskamele@gmail.com>
2023-02-04 21:28:01 +0200

    List Filtering with stream api

---
+++ src/main/java/T15_ListFiltering/Kata.java
@@ -0,0 +1,12 @@
+package T15_ListFiltering;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Kata {
+    public static List<Object> filterList(final List<Object> list) {
+        return list.stream()
+                .filter(element -> element instanceof Integer)
+                .collect(Collectors.toList());
+    }
+}
