91537c1ac7246a2eba284a4c437b3aa56dcf3f82
iskamele <iskamele@gmail.com>
2023-02-28 21:40:40 +0200

    Added clever solution for Opposites Attract.

---
+++ src/main/java/Kyu8/T02_OppositesAttract/OppositesAttractCleverSolution.java
@@ -0,0 +1,8 @@
+package Kyu8.T02_OppositesAttract;
+
+public class OppositesAttractCleverSolution {
+
+    public static boolean isLove(final int flower1, final int flower2) {
+        return flower1 % 2 != flower2 % 2;
+    }
+}
