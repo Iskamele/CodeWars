cf50619a45096de27bfad385dfebd321e88455f0
iskamele <iskamele@gmail.com>
2023-02-28 21:40:31 +0200

    Added new solution for Opposites Attract.

---
+++ src/test/java/Kyu8/T02_OppositesAttract/OppositesAttractTest.java
@@ -0,0 +1,29 @@
+package Kyu8.T02_OppositesAttract;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class OppositesAttractTest {
+
+    @Test
+    public void testOddAndEven() {
+        assertEquals(true, OppositesAttract.isLove(1, 4));
+    }
+
+    @Test
+    public void testEvenAndEven() {
+        assertEquals(false, OppositesAttract.isLove(2, 2));
+    }
+
+    @Test
+    public void testOddAndOdd() {
+        assertEquals(false, OppositesAttract.isLove(1, 1));
+    }
+
+    @Test
+    public void testEvenAndOdd() {
+        assertEquals(true, OppositesAttract.isLove(0, 1));
+    }
+
+}
