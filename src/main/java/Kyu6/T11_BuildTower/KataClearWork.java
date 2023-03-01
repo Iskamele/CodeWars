af293cb39a3fd3c12196a9e4939edd9409951287
iskamele <iskamele@gmail.com>
2023-03-01 23:03:20 +0200

    Added clear solution for Build Tower.

---
+++ src/main/java/Kyu6/T11_BuildTower/KataClearWork.java
@@ -0,0 +1,13 @@
+package Kyu6.T11_BuildTower;
+
+public class KataClearWork {
+    public static String[] TowerBuilder(int n) {
+        String[] t = new String[n];
+        String e;
+
+        for (int i = 0; i < n; i++)
+            t[i] = (e = " ".repeat(n - i - 1)) + "*".repeat(i + i + 1) + e;
+
+        return t;
+    }
+}
