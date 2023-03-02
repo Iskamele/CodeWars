ca734b3b6bd528a0031a67bca9e7a813027b1aeb
iskamele <iskamele@gmail.com>
2023-03-02 19:59:39 +0200

    Fixed tests.

--- src/test/java/Kyu6/T11_BuildTower/SolutionTest.java
+++ src/test/java/Kyu6/T11_BuildTower/SolutionTest.java
@@ -6,8 +6,8 @@ import static org.junit.Assert.assertEquals;
 public class SolutionTest {
   @Test
   public void basicTests() {
-    assertEquals(String.join(",", "*"), String.join(",", Kata.towerBuilder(1)));
-    assertEquals(String.join(",", " * ", "***"), String.join(",", Kata.towerBuilder(2)));
-    assertEquals(String.join(",", "  *  ", " *** ", "*****"), String.join(",", Kata.towerBuilder(3)));
+    assertEquals(String.join(",", "*"), String.join(",", KataStreamSolution.towerBuilder(1)));
+    assertEquals(String.join(",", " * ", "***"), String.join(",", KataStreamSolution.towerBuilder(2)));
+    assertEquals(String.join(",", "  *  ", " *** ", "*****"), String.join(",", KataStreamSolution.towerBuilder(3)));
   }
 }
