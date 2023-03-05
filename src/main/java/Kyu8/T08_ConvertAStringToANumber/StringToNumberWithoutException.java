b944f1bf97094c600f5611a72084e1481a719e3f
iskamele <iskamele@gmail.com>
2023-03-05 21:39:10 +0200

    Added impl for StringToNumber with exception.

---
+++ src/main/java/Kyu8/T08_ConvertAStringToANumber/StringToNumberWithoutException.java
@@ -0,0 +1,7 @@
+package Kyu8.T08_ConvertAStringToANumber;
+
+public class StringToNumberWithoutException {
+    public static int stringToNumber(String str) {
+        return Integer.parseInt(str);
+    }
+}
