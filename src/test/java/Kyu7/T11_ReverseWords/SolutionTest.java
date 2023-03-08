5d7d81ba175db5a4b9861cee6ae2b7f7d7a2d308
iskamele <iskamele@gmail.com>
2023-03-08 20:06:23 +0200

    Added test for Reverse Words.

---
+++ src/test/java/Kyu7/T11_ReverseWords/SolutionTest.java
@@ -0,0 +1,15 @@
+package Kyu7.T11_ReverseWords;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void exampleCases() {
+        assertEquals("ehT kciuq nworb xof spmuj revo eht yzal .god", Kata.reverseWords("The quick brown fox jumps over the lazy dog."));
+        assertEquals("elppa", Kata.reverseWords("apple"));
+        assertEquals("a b c d", Kata.reverseWords("a b c d"));
+        assertEquals("elbuod  decaps  sdrow", Kata.reverseWords("double  spaced  words"));
+    }
+}
