5a3bb6c5bb644956d4317064c79504ee7e4f91eb
Oleksandr Novikov <iskamele@gmail.com>
2025-07-27 20:30:55 +0200

    daily kata

--- src/main/java/Kyu4/T01_DecodeTheMorseCodeAdvanced/MorseCodeDecoder.java
+++ src/main/java/Kyu4/T01_DecodeTheMorseCodeAdvanced/MorseCodeDecoder.java
@@ -1,11 +1,71 @@
-package Kyu6.T14_DecodeTheMorseCode;
+package Kyu4.T01_DecodeTheMorseCodeAdvanced;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class MorseCodeDecoder {
+
     private static final String WORD_SEPARATOR = "\\s{3}";
     private static final String LETTER_SEPARATOR = "\\s{1}";
+
+    public static String decodeBits(String bits) {
+        Integer[] array = Arrays.stream(bits.chars().toArray()).boxed().toArray(Integer[]::new);
+
+        List<Integer> first = new ArrayList<>();
+        first.add(array[0]);
+        first.add(1);
+
+        List<List<Integer>> list = new ArrayList<>();
+        list.add(first);
+
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (Objects.equals(array[i], list.get(index).get(0))) {
+                List<Integer> list1 = list.get(index);
+                list.remove(list.size() - 1);
+                Integer last = list1.get(1);
+                last++;
+                list1.remove(list1.size() - 1);
+                list1.add(last);
+                list.add(list1);
+            } else {
+                ArrayList<Integer> newItem = new ArrayList<>();
+                newItem.add(array[i]);
+                newItem.add(1);
+                list.add(newItem);
+                index++;
+            }
+        }
+        StringBuilder builder = new StringBuilder();
+        for (List<Integer> integers : list) {
+            if (integers.get(0) == 49) {
+                if (integers.get(1) == 2) {
+                    builder.append(".");
+                }
+                if (integers.get(1) == 6) {
+                    builder.append("-");
+                }
+            }
+
+            if (integers.get(0) == 48) {
+                if (integers.get(1) == 6) {
+                    builder.append(" ");
+                }
+                if (integers.get(1) > 6) {
+                    builder.append("   ");
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+    public static String decodeMorse(String morseCode) {
+        return decode(morseCode);
+    }
 
     public static String decode(String morseCode) {
         String[] words = morseCode.trim().split(WORD_SEPARATOR);
